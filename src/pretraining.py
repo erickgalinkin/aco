fe34467f9c60744669697a2e95c727c5a6fbea34
Erick Galinkin <erick.galinkin@gmail.com>
2025-03-23 20:40:55 -0400

    Fix log message

--- src/pretraining.py
+++ src/pretraining.py
@@ -60,7 +60,7 @@ def run_training_example(
 
     cyborg = MultiAgentChallengeWrapper(env=CybORG(path, "sim", agents=agents))
 
-    logging.info(f"Starting training for {scenario}")
+    logging.info(f"Starting {player} player pretraining for {scenario}")
     for i in tqdm(range(max_eps), position=0):
         _ = cyborg.reset(player)
         rewards = {player: 0}
