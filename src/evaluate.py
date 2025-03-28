73b57052257feb82e88756dba123b65b70577182
Erick Galinkin <erick.galinkin@gmail.com>
2025-03-28 13:18:16 -0400

    Make evaluate.py reflect the official evaluation setting.

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,10 +1,12 @@
 import inspect
 import logging
 import json
+import numpy as np
 from pathlib import Path
 from tqdm import tqdm
 from CybORG import CybORG
-from CybORG.Agents import B_lineAgent, RedMeanderAgent
+from CybORG.Agents import B_lineAgent, RedMeanderAgent, SleepAgent
+from CybORG.Agents.Wrappers import ChallengeWrapper
 from wrappers import MultiAgentChallengeWrapper
 from agents import RedAgent, BlueAgent
 from gymnasium import spaces
@@ -26,11 +28,7 @@ parser.add_argument(
     "--model_path", type=str, help="Path to saved model files", required=True
 )
 parser.add_argument(
-    "--red_type",
-    choices=["bline", "meander", "ppo"],
-    type=str,
-    help="Type of red agent",
-    required=True,
+    "--ppo_only", action="store_true", help="Run evaluation on red/blue PPO only."
 )
 parser.add_argument("--scenario", type=str, help="Scenario to evaluate")
 
@@ -70,21 +68,13 @@ def get_sizes(scenario: str) -> Tuple[int, int, int, int]:
     )
 
 
-def load_agents(
-    red_params: dict, blue_params: dict
-) -> Tuple[BlueAgent, Union[RedAgent, BlueAgent, B_lineAgent, RedMeanderAgent]]:
+def load_agents(red_params: dict, blue_params: dict) -> Tuple[BlueAgent, RedAgent]:
     red_model_path = red_params["model_path"]
-    match red_model_path.lower():
-        case "bline":
-            red_agent = B_lineAgent
-        case "meander":
-            red_agent = RedMeanderAgent
-        case _:
-            red_agent = RedAgent(
-                action_size=red_params["action_size"],
-                state_size=red_params["state_size"],
-                model=red_model_path,
-            )
+    red_agent = RedAgent(
+        action_size=red_params["action_size"],
+        state_size=red_params["state_size"],
+        model=red_model_path,
+    )
 
     blue_agent = BlueAgent(
         action_size=blue_params["action_size"],
@@ -169,65 +159,73 @@ def evaluate_models(
             print(msg)
 
 
-def evaluate_blue(blue_model_path: str, red_type: str, scenario: str = "Scenario2"):
+def evaluate_blue(blue_model_path: str, scenario: str = "Scenario2"):
     logging.info(f"Starting evaluation for blue model on {scenario}")
-    logging.info(f"red type: {red_type}, blue model: {blue_model_path}")
+    logging.info(f"blue model: {blue_model_path}")
     model_id = blue_model_path.split("/")[-2].split("_")[0]
 
     red_action_size, red_state_size, blue_action_size, blue_state_size = get_sizes(
         scenario
     )
-    red_params = {
-        "model_path": red_type,
-    }
     blue_params = {
         "model_path": blue_model_path,
         "action_size": blue_action_size,
         "state_size": blue_state_size,
     }
-    red_agent, blue_agent = load_agents(red_params, blue_params)
+    blue_agent = BlueAgent(
+        action_size=blue_params["action_size"],
+        state_size=blue_params["state_size"],
+        model=blue_params["model_path"],
+    )
 
     path = str(inspect.getfile(CybORG))
     path = path[:-10] + f"/Shared/Scenarios/{scenario}.yaml"
 
+    total_rewards = dict()
+    total_actions = dict()
     for num_steps in [30, 50, 100]:
-        logging.info(f"Starting evaluation for {num_steps} steps")
-        cyborg = MultiAgentChallengeWrapper(
-            env=CybORG(path, "sim", agents={"Red": red_agent})
-        )
-        overall_rewards = list()
-        overall_actions = list()
-        for i in tqdm(range(MAX_EPS), position=0, leave=False):
-            rewards = list()
-            actions = list()
-            for j in tqdm(range(num_steps), position=1, leave=False):
-                observation = cyborg.reset("Blue")
-                action_space = cyborg.get_action_space("Blue")
-                action = blue_agent.get_action(observation, action_space)
-                next_observation, r, terminated, truncated, info = cyborg.step(
-                    agent="Blue", action=action
-                )
-                done = terminated or truncated
-                rewards.append(r)
-                actions.append(str(cyborg.get_last_action("Blue")))
-                if done:
-                    _ = cyborg.reset("Blue")
-                    break
-            overall_rewards.append(rewards)
-            overall_actions.append(actions)
-        data = {"rewards": overall_rewards, "actions": overall_actions}
-        Path(f"./logs/evaluation/{model_id}/").mkdir(parents=True, exist_ok=True)
-        try:
-            data_path = (
-                f"./logs/evaluation/{model_id}/{red_type}_{num_steps}_{scenario}.json"
+        for red_agent in [B_lineAgent, RedMeanderAgent, SleepAgent]:
+            cyborg = ChallengeWrapper(
+                env=CybORG(path, "sim", agents={"Red": red_agent}), agent_name="Blue"
             )
-            logging.info(f"Writing data to {data_path}")
-            with open(data_path, "w") as f:
-                json.dump(data, f)
-        except Exception as e:
-            msg = f"Exception while writing data: {e}"
-            logging.error(msg)
-            print(msg)
+            setting_rewards = list()
+            setting_actions = list()
+            observation = cyborg.reset()
+            action_space = cyborg.get_action_space("Blue")
+            for i in tqdm(range(MAX_EPS), position=0, leave=False):
+                rewards = list()
+                actions = {"Blue": list(), "Red": list()}
+                for j in tqdm(range(num_steps), position=1, leave=False):
+                    action = blue_agent.get_action(observation, action_space)
+                    observation, r, terminated, truncated, info = cyborg.step(action)
+                    done = terminated or truncated
+                    rewards.append(r)
+                    actions["Blue"].append(str(cyborg.get_last_action("Blue")))
+                    actions["Red"].append(str(cyborg.get_last_action("Red")))
+                    if done:
+                        _ = cyborg.reset("Blue")
+                        break
+                blue_agent.end_episode()
+                setting_rewards.append(sum(rewards))
+                setting_actions.append(actions)
+            print(
+                f"Average {num_steps} reward for red agent {red_agent.__name__}: {np.mean(setting_rewards)}"
+            )
+            total_rewards[f"{red_agent.__name__}_{num_steps}"] = np.mean(
+                setting_rewards
+            )
+            total_actions[f"{red_agent.__name__}_{num_steps}"] = setting_actions
+    data = {"rewards": total_rewards, "actions": total_actions}
+    Path(f"./logs/evaluation/{model_id}/").mkdir(parents=True, exist_ok=True)
+    try:
+        data_path = f"./logs/evaluation/{model_id}/{scenario}.json"
+        logging.info(f"Writing data to {data_path}")
+        with open(data_path, "w") as f:
+            json.dump(data, f)
+    except Exception as e:
+        msg = f"Exception while writing data: {e}"
+        logging.error(msg)
+        print(msg)
 
 
 if __name__ == "__main__":
@@ -236,11 +234,12 @@ if __name__ == "__main__":
         scenario = args.scenario
     else:
         scenario = "Scenario2"
-    if args.red_type == "ppo":
+    if args.ppo_only:
         red_model_path = f"{args.model_path}/red.ckpt"
         blue_model_path = f"{args.model_path}/blue.ckpt"
         evaluate_models(red_model_path, blue_model_path, scenario)
     else:
-        red_model_type = args.red_type
+        red_model_path = f"{args.model_path}/red.ckpt"
         blue_model_path = f"{args.model_path}/blue.ckpt"
-        evaluate_blue(blue_model_path, red_model_type, scenario)
+        evaluate_blue(blue_model_path, scenario)
+        evaluate_models(red_model_path, blue_model_path, scenario)
